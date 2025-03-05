1c2fd2962ba306589b8719c79a48c45f026535de
Arihant140206 <arihantjain140602@gmail.com>
2025-03-05 20:07:57 +0530

    Add files via upload

---
+++ averagePCM.java
@@ -0,0 +1,9 @@
+public class averagePCM{
+	public static void main(String[] args){
+	 int maths=94;
+	 int chem=95;
+	 int phy=96;
+	 double average = (maths + chem + phy )/3.0;
+	 System.out.println(average);
+	}
+}
