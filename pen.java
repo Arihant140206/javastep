08fa04585da01794119f158010589617e3867eb2
Arihant140206 <arihantjain140602@gmail.com>
2025-03-17 15:21:39 +0530

    Add files via upload

---
+++ pen.java
@@ -0,0 +1,14 @@
+public class pen{
+	public static void main (String[] args){
+		int totalpens = 14;
+		int students = 3 ;
+		int perstudent= totalpens/students ;
+		int rem = totalpens%students;
+		
+
+
+		System.out.println("The Pen Per Student is"+perstudent+ "and the remaining pen not distributed is" +rem);
+
+
+	}
+}
