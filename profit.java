3e648589b261da6de09042ea20f61fa8706231e3
Arihant140206 <arihantjain140602@gmail.com>
2025-03-18 08:39:43 +0530

    Add files via upload

---
+++ profit.java
@@ -0,0 +1,14 @@
+public class profit{
+	public static void main (String[] args){
+		int selling=191;
+		int cost=129;
+		int Profit = selling - cost ;
+        double ProfitPercentage = (Profit / (double)cost)  * 100;
+
+
+		System.out.println("The Cost Price is INR" +cost+"and Selling Price is INR"+selling +
+	    "The Profit is INR" +Profit +"+and the Profit Percentage is" +ProfitPercentage);
+
+
+	}
+}
