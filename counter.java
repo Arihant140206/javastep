57b755e782d02f25e48e07b104519bf6f81aae67
Arihant140206 <arihantjain140602@gmail.com>
2025-06-06 13:35:03 +0530

    Add files via upload

---
+++ counter.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class counter{
+public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+
+
+
+ for (int n = 10 ; n> 1 ; n--) {
+            System.out.println(n);
+        }
+    
+
+
+scanner.close();
+}
+}
