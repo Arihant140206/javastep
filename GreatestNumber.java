eb2218cd4dbea654d557b1e2d7cde4fbb28eb076
Arihant140206 <arihantjain140602@gmail.com>
2025-12-29 23:32:58 +0530

    Add files via upload

---
+++ GreatestNumber.java
@@ -0,0 +1,10 @@
+public class GreatestNumber {
+    public static int greatest(int a, int b, int c) {
+        if (a >= b && a >= c) return a;
+        else if (b >= c) return b;
+        else return c;
+    }
+    public static void main(String[] args) {
+        System.out.println(greatest(5, 8, 2)); // Example
+    }
+}
